c7b484358af26cad89aaeb48f6f83dab23374192
gold-olar <sam99kupo@gmail.com>
2020-05-13 00:49:30 +0100

    optimizing and building

---
+++ build/precache-manifest.ff5afcee98a449e5c89def1c86a5cfb8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cd223376096f2804227e578cb9577a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e81f4c558a90c95f97b",
+    "url": "/static/css/2.01a8e625.chunk.css"
+  },
+  {
+    "revision": "78afbfbca6820c3f9840",
+    "url": "/static/css/main.2179d115.chunk.css"
+  },
+  {
+    "revision": "9e81f4c558a90c95f97b",
+    "url": "/static/js/2.1e867bf0.chunk.js"
+  },
+  {
+    "revision": "7e8661572dbf62ca66d7837115f59618",
+    "url": "/static/js/2.1e867bf0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78afbfbca6820c3f9840",
+    "url": "/static/js/main.26e3d749.chunk.js"
+  },
+  {
+    "revision": "f30fd4ddfee1d119abd9",
+    "url": "/static/js/runtime-main.e2c85787.js"
+  },
+  {
+    "revision": "69fd69153443c0589267f7c06f214e87",
+    "url": "/static/media/banner.69fd6915.png"
+  },
+  {
+    "revision": "7b71b84d565d9c6f1eea962a134384c7",
+    "url": "/static/media/banner2.7b71b84d.png"
+  },
+  {
+    "revision": "8eb61905909e0882172ebb761349f2c9",
+    "url": "/static/media/footer_bg.8eb61905.png"
+  },
+  {
+    "revision": "7669c5f4839d73075b4c40967b5ca6c7",
+    "url": "/static/media/landing_illus.7669c5f4.png"
+  }
+]);
